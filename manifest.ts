f1406137adc827214848dd12bd45947845593fad
dailyco <dailyco03@gmail.com>
2023-07-19 23:22:17 +0900

    env: setting slack app

---
+++ manifest.ts
@@ -0,0 +1,16 @@
+import { Manifest } from "deno-slack-sdk/mod.ts";
+
+/**
+ * The app manifest contains the app's configuration. This
+ * file defines attributes like app name and description.
+ * https://api.slack.com/future/manifest
+ */
+export default Manifest({
+  name: "slack-lunch-bot-1",
+  description: "A blank template for building Slack apps with Deno",
+  icon: "assets/default_new_app_icon.png",
+  functions: [],
+  workflows: [],
+  outgoingDomains: [],
+  botScopes: ["commands", "chat:write", "chat:write.public"],
+});
